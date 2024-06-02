9168c8e4ddba1799d985685d77645d42b9a01a62
Samuel Jonathan <jonathandinesh@gmail.com>
2024-06-02 20:44:27 +0530

    Add webpack rules

--- src/app.tsx
+++
@@ -1,4 +0,0 @@
-import { createRoot } from 'react-dom/client';
-
-const root = createRoot(document.body);
-root.render(<h2 className="h-screen w-full text-center text-3xl font-bold underline bg-raisinblack text-brightorange">Hello from React!</h2>);
